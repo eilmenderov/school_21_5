4cb1dc7bd281322c88627aa54e8ce039bb7c390b
Verla Leida <vleida@si-n2.kzn.21-school.ru>
2022-01-05 16:37:33 +0300

    some fix's

--- cpp/module_00/ex00/megaphone.cpp
+++ cpp/module_00/ex00/megaphone.cpp
@@ -5,13 +5,11 @@ int main(int argc, char **argv)
 {
 	int i = 0;
 
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	while (argv[++i])
-	{
+	while (argv[++i]) {
 		int j = 0;
 		while (argv[i][j])
 			std::cout << (char)std::toupper(argv[i][j++]);
