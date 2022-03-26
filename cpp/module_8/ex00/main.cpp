193c1e406e1ad355140793a0c75ba81c7f47dec6
Evgeniy Ilmenderov <ilmenderov.e.v@gmail.com>
2022-03-27 02:42:36 +0300

    fix

--- cpp/module_8/ex00/main.cpp
+++ cpp/module_8/ex00/main.cpp
@@ -3,19 +3,19 @@
 int main() {
 
     srand(time(NULL));
-    int find_number = rand() % 100;
-    std::vector<int> vec_num;
-    vec_num.reserve(30);
+    int number = rand() % 100;
+    std::vector<int> vector;
+    vector.reserve(30);
     for (int i = 0; i < 30; i++)
-        vec_num.push_back(rand() % 100);
+        vector.push_back(rand() % 100);
     for (int i = 0; i < 30; i++)
-		std::cout << vec_num[i] << " ";
+		std::cout << vector[i] << " ";
 	std::cout << std::endl;
-	std::cout << "search: " << find_number << std::endl;
-    std::vector<int>::iterator it = easyfind(vec_num, find_number);
-	if (*it != find_number)
-		std::cout << "match not found: " << find_number << std::endl;
+	std::cout << "search: " << number << std::endl;
+    std::vector<int>::iterator it = easyfind(vector, number);
+	if (*it != number)
+		std::cout << "match not found: " << number << std::endl;
 	else
-		std::cout << "match found: " << find_number << std::endl;
+		std::cout << "match found: " << number << std::endl;
     return (0);
 }
