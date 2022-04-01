2159192eabdc24cc6f90c935f6da41e2651cad37
Evgeniy Ilmenderov <ilmenderov.e.v@gmail.com>
2022-04-01 03:10:01 +0300

    finish cpp-modules

--- cpp/module_8/ex02/main.cpp
+++ cpp/module_8/ex02/main.cpp
@@ -7,9 +7,9 @@ int main() {
         MutantStack<int> mstack;
         mstack.push(5);
         mstack.push(17);
-        std::cout << mstack.top() << std::endl;
+        std::cout << "top: " << mstack.top() << std::endl;
         mstack.pop();
-        std::cout << mstack.size() << std::endl;
+        std::cout << "size: "<< mstack.size() << std::endl;
         mstack.push(3);
         mstack.push(5);
         mstack.push(737);
